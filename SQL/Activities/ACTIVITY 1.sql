1840755ed14aa08ef4b487b33852d403f6c0f956
Ravikumar-IBM <136794142+Ravikumar-IBM@users.noreply.github.com>
2023-06-21 20:04:49 +0530

    Add files via upload

---
+++ SQL/Activities/ACTIVITY 1.sql
@@ -0,0 +1,13 @@
+REM   Script: ACTIVITY 1
+REM   ACTIVITY 1_SQL
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+DESCRIBE salesman
+
+
