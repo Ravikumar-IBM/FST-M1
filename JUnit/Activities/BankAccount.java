3a64f35ab6a89eb0052f4258a879905ba322b265
Ravikumar-IBM <136794142+Ravikumar-IBM@users.noreply.github.com>
2023-07-12 11:09:40 +0530

    Add files via upload

---
+++ JUnit/Activities/BankAccount.java
@@ -0,0 +1,16 @@
+package Activites;
+
+public class BankAccount {
+    private Integer balance;
+    public BankAccount(Integer initialBalance) {
+        balance = initialBalance;
+    }
+    public Integer withdraw(Integer amount) {
+        if (balance < amount) {
+            throw new NotEnoughFundsException(amount, balance);
+        }
+        balance -= amount;
+        return balance;
+    }
+}
+
---
+++ JUnit/Activities/activity2.java
@@ -0,0 +1,22 @@
+package Activites;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class activity2 {
+
+    @Test
+    void notEnoughFunds() {
+        BankAccount account = new BankAccount(9);
+        assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10),
+                "Balance must be greater than amount of withdrawal");
+    }
+
+    @Test
+    void enoughFunds() {
+        BankAccount account = new BankAccount(100);
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
+}
